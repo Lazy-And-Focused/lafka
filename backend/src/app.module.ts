9d5ffa730a0a511e86ea1f65be8c4c261ca1aab5
FOCKUSTY <wwddwwwd@mail.ru>
2024-09-08 14:26:38 +0500

    -new backend init

---
+++ backend/src/app.module.ts
@@ -0,0 +1,13 @@
+import { Module, NestModule, MiddlewareConsumer } from '@nestjs/common';
+import { LoggerMiddleware } from './middleware/logger.middleware';
+
+@Module({
+    imports: []
+})
+export class AppModule implements NestModule {
+    configure(consumer: MiddlewareConsumer) {
+        consumer
+            .apply(LoggerMiddleware)
+            .forRoutes('/');
+    };
+};
