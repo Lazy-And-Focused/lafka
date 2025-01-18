5d4b959ee55c30d81b0a57c0c9c13086c3deac47
FOCKUSTY <wwddwwwd@mail.ru>
2025-01-18 22:26:39 +0500

    revert commit

--- packages/types/src/index.ts
+++
@@ -1,6 +0,0 @@
-export * from "./authors/user.types";
-export * from "./content/comment.types";
-export * from "./posts/blog-post.types";
-export * from "./posts/forum-post.types";
-export * from "./posts/post.types";
-export * from "./utility/utility.types";
