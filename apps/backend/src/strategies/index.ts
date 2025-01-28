7b0eadbfde5626ff677936b23dae5c371b09f0ea
FOCKUSTY <wwddwwwd@mail.ru>
2025-01-28 19:00:28 +0500

    -ref

--- apps/backend/src/strategies/index.ts
+++ apps/backend/src/strategies/index.ts
@@ -1,15 +1,10 @@
 import GeneralStrategy from "./general.stategy";
-// import GooglePassport from "./google-auth.strategy";
-// import YandexPassport from "./yandex-auth.strategy";
 
 class Passport extends GeneralStrategy {
 	public constructor() {
 		super();
 
-		this.defaultInitialize();
-
-		// new GooglePassport();
-		// new YandexPassport();
+		this.auth.init();
 	}
 }
 
