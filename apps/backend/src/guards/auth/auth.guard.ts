38c35bc1e28ff890471bd503ec9e75727c052e89
FOCKUSTY <wwddwwwd@mail.ru>
2025-02-27 22:38:33 +0500

    -hot-fix

--- apps/backend/src/guards/auth/auth.guard.ts
+++ apps/backend/src/guards/auth/auth.guard.ts
@@ -1,4 +1,5 @@
 import { Injectable, CanActivate, ExecutionContext } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
 import { Request } from 'express';
 import { Observable } from 'rxjs';
 
@@ -6,9 +7,15 @@ import Service from './auth-guard.service';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
+    constructor(private readonly reflector: Reflector) {
+    }
+    
     canActivate(
         context: ExecutionContext,
     ): boolean | Promise<boolean> | Observable<boolean> {
+        if (this.reflector.get<boolean>('isPublic', context.getHandler()))
+            return true;
+
         const request = context.switchToHttp().getRequest<Request>();
 
         return new Service().validateRequest(request);
