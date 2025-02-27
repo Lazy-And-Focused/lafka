ddd2948d0460b336fe57727199642b8ea3b5cea8
FOCKUSTY <wwddwwwd@mail.ru>
2025-02-27 22:21:04 +0500

    -up auth.guard

--- apps/backend/src/guards/auth/auth-guard.service.ts
+++ apps/backend/src/guards/auth/auth-guard.service.ts
@@ -1,7 +1,24 @@
 import { Request } from "express";
 
+import Database from "lafka/database/database/models.database";
+import { AuthUser } from "lafka/types/auth/auth-user.types";
+
+const { auth_users, users } = Database;
+
 class Service {
-    public validateRequest(request: Request) {
+    public async validateRequest(req: Request) {
+		const user = req.user as AuthUser;
+		if (!user) return false;
+		
+		const findedUser = await auth_users.model.findOne({id: user.id});
+		
+		if (!findedUser) return false;
+		if (user.access_token !== findedUser.access_token) return false;
+		
+		const profileUser = await users.model.findOne({id: user.id});
+
+		if (!profileUser) return false;
+
 		return true;
 	}
 }
