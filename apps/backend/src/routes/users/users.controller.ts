e2d48875b7a8af39b2902f14413247a4fd502814
FOCKUSTY <wwddwwwd@mail.ru>
2025-02-27 21:05:57 +0500

    -up finding

--- apps/backend/src/routes/users/users.controller.ts
+++ apps/backend/src/routes/users/users.controller.ts
@@ -4,18 +4,25 @@ import { Controller, Get, Injectable, Param, UseGuards } from "@nestjs/common";
 import { UsersService } from "./users.service";
 import { AuthGuard } from "guards/auth/auth.guard";
 
+const starts = /username|id/;
+
 @Injectable()
 @Controller("users")
 @UseGuards(AuthGuard)
 export class UsersController {
     public constructor(private usersService: UsersService) {}
 
-    @Get(":id")
-    public async get(@Param("id") id: string) {
-        const data = await this.usersService.getUser(id);
+    @Get(":data")
+    public async get(@Param("data") data: string) {
+        const matchs = data.match(starts);
+        const d = matchs
+            ? {[matchs[0]]: data.split("-")[1]}
+            : { id: data };
+
+        const res = await this.usersService.getUser(d);
 
         return {
-            ...data,
+            ...res,
             type: "user"
         };
     }
--- apps/backend/src/routes/users/users.service.ts
+++ apps/backend/src/routes/users/users.service.ts
@@ -9,9 +9,9 @@ const { users } = Database;
 
 @Injectable()
 export class UsersService {
-    public async getUser(id: string): Promise<ServiceResponse<User>> {
+    public async getUser(data: Partial<User>|string): Promise<ServiceResponse<User>> {
         try {
-            const user = await users.model.findOne({id});
+            const user = await users.model.findOne(typeof data === "string" ? {id: data} : data);
 
             if (!user)
                 return { successed: false, error: "User not found" };
