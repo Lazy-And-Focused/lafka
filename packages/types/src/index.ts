43fe1b600b14c885f8c8301460e735ec5368a256
FOCKUSTY <wwddwwwd@mail.ru>
2025-03-20 15:07:33 +0500

    -add keys to namespace

--- packages/types/src/index.ts
+++ packages/types/src/index.ts
@@ -1,13 +1,44 @@
-import { User as UserT } from "./authors/user.types";
-import { Comment as CommentT } from "./content/comment.types";
-import { BlogPost as BlogPostT } from "./posts/blog-post.types";
-import { ForumPost as ForumPostT } from "./posts/forum-post.types";
-import { Post as PostT } from "./posts/post.types";
+import {
+    authTypes as authTypesC,
+    AuthTypes as AuthTypesT,
+    AuthUser as AuthUserT,
+    AUKeys as CAUKeys
+} from "./auth/auth-user.types";
+import {
+    BlogPost as BlogPostT,
+    BPKeys as CBPKeys,
+} from "./posts/blog-post.types";
+import {
+    Comment as CommentT,
+    CKeys as CCKeys,
+} from "./content/comment.types";
+import {
+    ForumPost as ForumPostT,
+    PFKeys as CPFKeys,
+} from "./posts/forum-post.types";
+import {
+    Post as PostT,
+    PKeys as CPKeys,
+} from "./posts/post.types";
+import {
+    User as UserT,
+    UKeys as CUKeys,
+} from "./authors/user.types";
+
 import { Link as LinkT, PostStatus as PostStatusT, Tag as TagT, Tags as TagsT } from "./utility/utility.types";
-import { authTypes as authTypesC, AuthTypes as AuthTypesT, AuthUser as AuthUserT } from "./auth/auth-user.types";
 
 export namespace LAFka {
     export const authTypes = authTypesC;
+
+    export const keys = {
+        auth_users: CAUKeys,
+        posts: [...CPKeys, ...CPFKeys, ...CBPKeys],
+        comments: CCKeys,
+        users: CUKeys,
+        
+        blog_posts: CBPKeys,
+        forum_posts: CPFKeys,
+    } as const;
 
     export type AuthTypes = AuthTypesT;
     export type AuthUser = AuthUserT;
