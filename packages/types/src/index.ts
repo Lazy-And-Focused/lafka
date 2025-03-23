2f67f0884d289849d8e1378b9ae559c0ff3f3fd6
FOCKUSTY <wwddwwwd@mail.ru>
2025-03-24 00:40:29 +0500

    -up to types namespace

--- packages/types/src/index.ts
+++ packages/types/src/index.ts
@@ -11,13 +11,9 @@ import {
 } from "./auth/auth-user.types";
 
 import { BlogPost as IBlogPost } from "./posts/blog-post.types";
-
 import { Comment as IComment } from "./content/comment.types";
-
 import { ForumPost as IForumPost } from "./posts/forum-post.types";
-
 import { Post as IPost } from "./posts/post.types";
-
 import { User as IUser } from "./authors/user.types";
 
 import {
@@ -37,12 +33,50 @@ import {
   DataType as TDataType
 } from "./backend/data.types";
 
+import {
+  R_KEYS as CR_KEYS,
+  LAZY_RIGHTS as CLAZY_RIGHTS,
+  DEFAULT_USER_RIGHTS as CDEFAULT_USER_RIGHTS,
+  
+  LazyRights as ILazyRights,
+  Rights as IRights,
+
+  LazyMeRights as TLazyMeRights,
+  LazyPostsRights as TLazyPostsRights,
+  LazyUsersRights as TLazyUsersRights,
+  LazyOrganizationsRights as TLazyOrganizationsRights,
+  
+  MeRights as TMeRights,
+  PostsRights as TPostsRights,
+  UsersRights as TUsersRights,
+  OrganizationsRights as TOrganizationsRights,
+} from "./authors/rights.types";
+
 export namespace LAFka.Response {
   export type GetData<T> = IGetData<T>;
   export type CreateData<T> = ICreateData<T>;
   export type ChangeData<T> = IChangeData<T>;
   export type DeleteData<T> = IDeleteData<T>;
   export type DataType = TDataType;
+}
+
+export namespace LAFka.Rights {
+  export const R_KEYS = CR_KEYS;
+  export const LAZY_RIGHTS = CLAZY_RIGHTS;
+  export const DEFAULT_USER_RIGHTS = CDEFAULT_USER_RIGHTS;
+
+  export type LazyRights = ILazyRights;
+  export type Rights = IRights;
+  
+  export type LazyMeRights = TLazyMeRights;
+  export type LazyPostsRights = TLazyPostsRights;
+  export type LazyUsersRights = TLazyUsersRights;
+  export type LazyOrganizationsRights = TLazyOrganizationsRights;
+  
+  export type MeRights = TMeRights;
+  export type PostsRights = TPostsRights;
+  export type UsersRights = TUsersRights;
+  export type OrganizationsRights = TOrganizationsRights;
 }
 
 export namespace LAFka {
