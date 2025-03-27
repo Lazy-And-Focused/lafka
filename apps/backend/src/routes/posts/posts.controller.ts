338a232b4627f8873955bc658d5859b1edd76752
FOCKUSTY <wwddwwwd@mail.ru>
2025-03-27 18:51:11 +0500

    -add posts router

---
+++ apps/backend/src/routes/posts/posts.controller.ts
@@ -0,0 +1,102 @@
+import { LAFka } from "lafka/types";
+import DB from "lafka/database";
+
+import { Request } from "express";
+import {
+  Controller,
+  Get,
+  Inject,
+  Injectable,
+  Param,
+  Post,
+  Query,
+  Req,
+  UseGuards
+} from "@nestjs/common";
+import { CACHE_MANAGER } from "@nestjs/cache-manager";
+import { Cache } from "cache-manager";
+
+import { POSTS_CONTROLLER, POSTS_ROUTES } from "./posts.routes";
+import { PostsService } from "./posts.service";
+
+import { AuthGuard } from "guards/auth/auth.guard";
+
+import Hash from "api/hash.api";
+
+@Injectable()
+@Controller(POSTS_CONTROLLER)
+@UseGuards(AuthGuard)
+export class PostsContoller {
+  public constructor(
+    private readonly postsService: PostsService,
+    @Inject(CACHE_MANAGER) private cacheManager: Cache
+  ) {}
+
+  @Get(POSTS_ROUTES.GET)
+  public async getPosts(
+    @Req() req: Request,
+    @Query("offset") offset?: string,
+    @Query("count") count?: string,
+    @Query("sortBy") sortBy?: string,
+    @Query("sortType") sortType?: string
+  ): Promise<LAFka.Response.GetData<LAFka.Post[]>> {
+    const { successed } = Hash.parse(req);
+
+    if (!successed)
+      return {
+        successed: false,
+        type: "posts",
+        resource: []
+      };
+
+    const posts = await this.postsService.getPosts({
+      offset, count, sortBy, sortType
+    });
+
+    return {...posts, type: "posts" }
+  }
+
+  @Get(POSTS_ROUTES.GET_ONE)
+  public async getPost(
+    @Req() req: Request,
+    @Param("id") id: string
+  ): Promise<LAFka.Response.GetData<LAFka.Post>> {
+    const { successed } = Hash.parse(req);
+
+    if (!successed)
+      return {
+        successed: false,
+        type: "posts"
+      };
+
+    const post = await this.postsService.getPost(id);
+
+    return {...post, type: "posts"};
+  }
+
+  @Post(POSTS_ROUTES.POST)
+  public async createPost(
+    @Req() req: Request,
+  ): Promise<LAFka.Response.CreateData<LAFka.Post>> {
+    const date = new Date();
+    const { successed, profile_id } = Hash.parse(req);
+    
+    if (!successed)
+      return {
+        date,
+        successed: false,
+        type: "posts"
+      };
+
+    const body = DB.Database.parse(req.body, "posts");
+    const post = await this.postsService.createPost(profile_id, body);
+
+    return {
+      successed: post.successed,
+      created_resource: post.resource,
+      error: post.error,
+      date,
+      type: "posts"
+    };
+  }
+}
---
+++ apps/backend/src/routes/posts/posts.service.ts
@@ -0,0 +1,124 @@
+import { Injectable } from "@nestjs/common";
+
+import { Constructors } from "lafka/database/database/models.database";
+import DB, { Models } from "lafka/database";
+import { LAFka, Rights as RightsTypes } from "lafka/types";
+import { Rights } from "@lafka/rights";
+
+import { UpdateWriteOpResult } from "mongoose";
+import { DeleteResult } from "lafka/database/types/mongodb.types";
+import { ServiceResponse } from "lafka/types/service.types";
+
+const { posts, users } = new Models();
+
+const FILTER = {
+  offset: 0,
+  count: 0,
+  sortBy: ["likes", "dislikes", "followers", "created_at", "changed_at"],
+  sortType: ["asc", "desc", 1, -1, true, false]
+};
+
+const REQUIRED_DATA_TO_CREATE_POST = [
+  "content",
+  "creator_id",
+  "name",
+  "type"
+];
+
+export type Filter = {
+  offset: number,
+  count: number,
+  sortBy: "likes"|"dislikes"|"followers"|"created_at"|"changed_at",
+  sortType: 1 | -1
+}
+
+@Injectable()
+export class PostsService {
+  public async getPosts(rawFilter: Record<string, string>): Promise<ServiceResponse<LAFka.Post[]>> {
+    const filter = this.parseFilter({
+      count: "0",
+      offset: "0",
+      sortBy: "created_at",
+      sortType: "desc",
+      ...rawFilter
+    });
+
+    const data = await posts.getData({filter: {}, options: {
+      limit: filter.count,
+      skip: filter.offset,
+      sort: { [filter.sortBy]: filter.sortType }
+    }});
+
+    if (!data.successed || !data.data) return { successed: false, error: data.text };
+    if (data.data.length === 0) return { successed: false, resource: [], error: "Posts not found" };
+
+    return {
+      successed: true,
+      resource: data.data.map(p => DB.Database.parse(p, "posts"))
+    }
+  }
+
+  public async getPost(id: string): Promise<ServiceResponse<LAFka.Post>> {
+    const data = await posts.getData({filter: {id}});
+    
+    if (!data.successed || !data.data || data.data.length === 0)
+      return { successed: false, error: "Post not found" };
+  
+    return {
+      successed: true,
+      resource: data.data[0]
+    };
+  }
+
+  public async createPost(userId: string, post: Constructors.posts): Promise<ServiceResponse<LAFka.Post>> {
+    const user = (await users.getData({filter: {id: userId}})).data;
+
+    if (!user || !user[0]) return { successed: false, error: "User not found"};
+
+    if (!new Rights.User(user[0]).defaultHas(RightsTypes.Default.ME_RIGHTS.POSTS_CREATE))
+      return { successed: false, error: "403 Forbidenn" };
+
+    for (const required of REQUIRED_DATA_TO_CREATE_POST) {
+      if (!Object.keys(post).includes(required)) {
+        return {
+          successed: false,
+          error: "there is no required data"
+        };
+      }
+    }
+
+    return {
+      successed: true,
+      resource: DB.Database.parse(new DB.Post(post), "posts")
+    };
+  }
+
+  public async putPost(user: LAFka.User): Promise<ServiceResponse<LAFka.Post>> {
+    return { successed: false, error: "the method has not been initialized" }
+  }
+
+  public async deletePost(user: LAFka.User): Promise<ServiceResponse<LAFka.Post>> {
+    return { successed: false, error: "the method has not been initialized" }
+  }
+
+  private parseFilter(data: Record<keyof Filter, string>): Filter {
+    return {
+      count: Number(data.count),
+      offset: Number(data.offset),
+      sortBy: FILTER.sortBy.includes(data.sortBy) ? data.sortBy as Filter["sortBy"] : "created_at",
+      sortType: typeof data.sortType === "boolean"
+        ? data.sortType === true
+          ? 1
+          : -1
+        : typeof data.sortType === "number"
+          ? data.sortType === 1
+            ? 1
+            : -1
+          : typeof data.sortType === "string"
+            ? data.sortType === "asc"
+              ? 1
+              : -1
+            : -1
+    };
+  }
+}
