d8582040a5ce7a4d6a6ab9c0b5c015e43a46d47d
lanvalird <strazhognay@gmail.com>
2025-03-30 12:25:29 +0400

    -fix Save User in CacheStore and conditions in UserPanel component

--- apps/frontend/src/app/components/Header__UserPanel.tsx
+++ apps/frontend/src/app/components/Header__UserPanel.tsx
@@ -5,19 +5,45 @@ import FilePencil from '../icons/FilePencil';
 import { useEffect, useState } from 'react';
 import { redirect } from 'next/navigation';
 import { getUserFromSource } from '../../utils/getUser';
+import { LAFka } from '@lafka/types';
 
 export default function Header__UserPanel() {
-  const [user, setUser] = useState<string | undefined>(undefined);
+  const [user, setUser] = useState<LAFka.User | null>(null);
+  const [isLoading, setLoading] = useState<boolean>(true);
+  const [isError, setError] = useState<boolean>(false);
 
   useEffect(() => {
     const fetchData = async () => {
-      const user = await getUserFromSource();
+      const res = await getUserFromSource();
 
-      setUser(user?.username);
+      if (res.error) setError(true);
+      else {
+        setUser(res.result);
+        setLoading(false);
+      }
     };
 
     fetchData();
   }, []);
+
+  if (isError || isLoading)
+    return (
+      <div
+        className={
+          'flex flex-row items-center justify-end gap-5 ' +
+          (isLoading && !isError ? 'animate-pulse' : '')
+        }
+      >
+        <Image
+          className='aspect-square h-8 w-8 rounded-full'
+          src='https://laf-info.netlify.app/images/avatars/default.png'
+          alt={'Loading avatar'}
+          width='32'
+          height='32'
+        />
+        Inkognito ({isError ? 'Error' : 'Logging...'})
+      </div>
+    );
 
   if (!user)
     return (
@@ -50,12 +76,15 @@ export default function Header__UserPanel() {
         </button>
         <Image
           className='aspect-square h-8 w-8 rounded-full'
-          src={'https://laf-info.netlify.app/images/avatars/default.png'}
-          alt={'Logotype'}
+          src={
+            user.avatar ||
+            'https://laf-info.netlify.app/images/avatars/default.png'
+          }
+          alt={`${user.username}'s avatar`}
           width='32'
           height='32'
         />
-        {user}
+        {user.nickname || user.username}
       </div>
     );
 }
--- apps/frontend/src/utils/getUser.ts
+++ apps/frontend/src/utils/getUser.ts
@@ -1,25 +1,30 @@
 'use server';
 
-import { cookies } from 'next/headers';
+import { LAFka } from '@lafka/types';
+import validateCookie from './validateCookies';
 
-async function validateCookie() {
-  const cookieStore = await cookies();
-  const token = cookieStore.get('id-token');
+export async function getUserFromSource(): Promise<{
+  result: LAFka.User | null;
+  error: boolean;
+}> {
+  const token = await validateCookie('id-token');
 
-  return token ? token.value : false;
-}
+  if (!token) return { result: null, error: false };
 
-export async function getUserFromSource(): Promise<any> {
-  const token = await validateCookie();
+  try {
+    const res = await fetch('http://localhost:3001/api/users/', {
+      method: 'GET',
+      headers: {
+        token,
+      },
+      cache: 'force-cache',
+      next: {
+        revalidate: 10_800, // 3 hours
+      },
+    }).then(async (data) => await data.json());
 
-  if (!token) return null;
-
-  const res = await fetch('http://localhost:3001/api/users/', {
-    method: 'GET',
-    headers: {
-      token,
-    },
-  }).then(async (data) => await data.json());
-
-  return res.resource;
+    return { result: res.resource, error: false };
+  } catch (e) {
+    return { result: null, error: true };
+  }
 }
