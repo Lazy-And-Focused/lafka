0e26c6746b0b3a14d8944dc566768675b6f86302
FOCKUSTY <116993925+FOCKUSTY@users.noreply.github.com>
2025-03-30 12:04:16 +0500

    -up types -> parse.helper

--- packages/db-logic/src/classes/helpers/parse.helper.ts
+++ packages/db-logic/src/classes/helpers/parse.helper.ts
@@ -3,7 +3,7 @@ import { LAFka } from "lafka/types";
 
 const parse = <T extends { id: string }>(data: T, type: Schemas.Models): T => {
   const output: { [key: string]: unknown } = {};
-  const keys = LAFka.KEYS[type];
+  const keys = LAFka.Database.KEYS[type];
   keys.forEach((k: string) => {
     output[k] = (data as { [key: string]: unknown })[k];
   });
