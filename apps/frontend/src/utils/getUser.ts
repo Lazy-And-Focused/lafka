59c4631e0c5a83252f79ea122abe85949baf1080
lanvalird <strazhognay@gmail.com>
2025-04-05 16:56:57 +0400

    -up DefaultUser Logic

--- apps/frontend/src/utils/getUser.ts
+++ apps/frontend/src/utils/getUser.ts
@@ -2,6 +2,7 @@
 
 import { LAFka } from '@lafka/types';
 import validateCookie from './validateCookies';
+import DefaultUser from './_test/DefaultUser';
 
 export async function getUserFromSource(): Promise<{
   result: LAFka.User | null;
@@ -12,6 +13,9 @@ export async function getUserFromSource(): Promise<{
   if (!token) return { result: null, error: false };
 
   try {
+    if (process.env.NODE_ENV === 'development' && process.env.ENABLE_TEST_USER)
+      return { result: DefaultUser, error: false };
+
     const res = await fetch(process.env.NEXT_PUBLIC_BACKEND_API + '/users/', {
       method: 'GET',
       headers: {
