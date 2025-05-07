c6a0cdac7207d47cc5e791033db81400ef473a28
lanvalird <strazhognay@gmail.com>
2025-05-07 16:07:14 +0400

    -up-fix use-user

--- apps/frontend/src/shared/user/hooks/use-user.ts
+++ apps/frontend/src/shared/user/hooks/use-user.ts
@@ -2,19 +2,19 @@
 
 import { LAFka } from '@lafka/types';
 import { useState, useEffect } from 'react';
-import getUser from './getUser';
+import getUser from './get-user';
 
-export default function useUser(cached: boolean = true) {
+export default function useUser() {
   const [user, setUser] = useState<LAFka.User | null>(null);
 
   useEffect(() => {
     async function setter() {
-      const value = await getUser(cached);
+      const value = await getUser();
       setUser(value);
     }
 
     setter();
-  }, [cached]);
+  }, []);
 
   return user;
 }
