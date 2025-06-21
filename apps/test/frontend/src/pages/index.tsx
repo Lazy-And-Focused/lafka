60cf4702ae68137f038e3099d13d80aed7f571da
lanvalird <strazhognay@gmail.com>
2025-06-21 13:31:41 +0400

    -up css in test-frontend

--- apps/test/frontend/src/pages/index.tsx
+++ apps/test/frontend/src/pages/index.tsx
@@ -1,69 +1,66 @@
-import { GetServerSidePropsContext, NextPage } from "next";
+import { LAFka } from '@lafka/types';
 
-import { User } from "@/components/user/user.component";
-import { LAFka } from "@lafka/types";
-import { validateCookies } from "@/api/validator";
-import { Post } from "@/components/posts/post.component";
+import { GetServerSidePropsContext, NextPage } from 'next';
+
+import { User } from '@/components/user/user.component';
+import { Post } from '@/components/posts/post.component';
+import { validateCookies } from '@/api/validator';
+
+import Link from 'next/link';
+
+import './main.css';
 
 /* eslint-disable */
 type Props = {
-    user?: LAFka.User,
-    headers?: any
+  user?: LAFka.User;
+  headers?: any;
 };
- /* eslint-enable */
+/* eslint-enable */
+
+const authMethods = LAFka.AUTH_TYPES;
 
 const Home: NextPage<Props> = ({ user, headers }) => {
-    return (
-        <div>
-            <div>
-                <button onClick={() => window.location.href = "http://localhost:3001/api/auth/google"}>
-                    Google register
-                </button>
-                <button onClick={() => window.location.href = "http://localhost:3001/api/auth/yandex"}>
-                    Yandex register
-                </button>
-            </div>
-            
-            <br />
+  return (
+    <>
+      <div className='auth-btns-wrapper'>
+        <h1 style={{ textAlign: 'center', marginBottom: '0.5rem' }}>
+          Войти через
+        </h1>
 
-            <div id="create" style={{display: "flex", backgroundColor: "#999099", padding: "2em", gap: "3em"}}>
-                <div style={{width: "300px", height: "700px"}}>
-                    <span>Your profile:</span>
-                    <br />
-                    <User user={user} />
-                </div>
+        {authMethods.map((method) => (
+          <button className='btn btn-auth'>
+            <Link href={`${process.env.NEXT_BACKEND_URL}/api/auth/${method}`}>
+              {method.toUpperCase()}
+            </Link>
+          </button>
+        ))}
+      </div>
 
-                <div>
-                    {
-                        (user && headers) 
-                            ? <Post userId={user.id} headers={headers}></Post>
-                            : <span>You must register</span>
-                    }
-                </div>
-            </div>
-            <div id="posts">
-
-            </div>
-        </div>
-    )
+      {user && <User user={user} />}
+      {/* {user && headers && <Post userId={user.id} headers={headers} />} */}
+    </>
+  );
 };
 
-export const getServerSideProps = async(ctx: GetServerSidePropsContext): Promise<{ props: Props; }> => {
-    const headers = validateCookies(ctx);
+export const getServerSideProps = async (
+  ctx: GetServerSidePropsContext,
+): Promise<{ props: Props }> => {
+  const headers = validateCookies(ctx);
 
-    if(!headers)
-        return {props: {}};
+  if (!headers) return { props: {} };
 
-    const user = await fetch("http://localhost:3001/api/users/", {headers});
+  const user = await fetch(`${process.env.NEXT_BACKEND_URL}/api/users/`, {
+    headers,
+  });
 
-    if (user.status !== 200) return { props: {}};
+  if (user.status !== 200) return { props: {} };
 
-    return {
-        props: {
-            user: (await user.json()).resource,
-            headers,
-        }
-    };
+  return {
+    props: {
+      user: (await user.json()).resource,
+      headers,
+    },
+  };
 };
 
 export default Home;
