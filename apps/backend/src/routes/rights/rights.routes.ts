910d576250d61f940245d58ca2fb8136aeec330c
FOCKUSTY <116993925+FOCKUSTY@users.noreply.github.com>
2025-06-29 02:30:27 +0500

    -rm rights route

--- apps/backend/src/routes/rights/rights.routes.ts
+++
@@ -1,8 +0,0 @@
-export const RIGHTS_CONTROLLER = ["rights", "r"] as const;
-
-export const RIGHTS_ROUTES = {
-  GET_MY: "",
-  GET: ":method/:id",
-  PUT: ":method/:id",
-  DELETE: ":method/:id",
-} as const;
