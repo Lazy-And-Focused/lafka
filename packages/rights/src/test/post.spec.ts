bac7818525c9a085752209e3156485106e893ed1
FOCKUSTY <wwddwwwd@mail.ru>
2025-09-09 14:02:43 +0500

    -up packages/rights

---
+++ packages/rights/src/test/post.spec.ts
@@ -0,0 +1,24 @@
+import { PostService } from "../index";
+
+import { user, post } from "./constants";
+import Test from "./test.class";
+
+const rightService = new PostService(post);
+
+new Test("Post test", [
+  {
+    must: true,
+    name: "Owner rights",
+    returned: rightService.hasRights("OWNER")(user.id)
+  },
+  {
+    must: false,
+    name: "Block user",
+    returned: rightService.hasRights("COMMENTS_CREATE", "ATTACH_FILES", "VIEWERS_MUTE", "REACT")("3")
+  },
+  {
+    must: true,
+    name: "Manager rights",
+    returned: rightService.userHas("2")("MANAGER"),
+  }
+]).execute();
--- packages/rights/src/types.ts
+++
@@ -1,4 +0,0 @@
-export type MustArray<T, K=T> = [T, ...K[]];
-export type ArrayOrType<T> = MustArray<T> | T;
-
-export type Bit = bigint|number|string|boolean;
