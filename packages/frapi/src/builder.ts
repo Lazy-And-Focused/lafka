382b74017fd5d4aa4f1e7851d6927e4c876c6c5e
FOCKUSTY <wwddwwwd@mail.ru>
2025-09-20 13:50:53 +0500

    -up routes

--- packages/frapi/src/builder.ts
+++ packages/frapi/src/builder.ts
@@ -2,4 +2,27 @@ import Compiler from "fouter";
 
 import { join } from "path";
 
-new Compiler(__dirname, join(__dirname, "routes.ts")).execute();
+const compiler = new Compiler(__dirname, join(__dirname, "routes.ts"));
+const data = compiler.execute(false);
+
+const COMPILED_OVERWRRITE_TEXT = `// FOUTER__COMPILED__OVERWRITE_THIS \\\\`;
+
+const routes = JSON.stringify(
+  Object.fromEntries(
+    data.map((route) => {
+      return [
+        `'${route.parent}'`,
+        Object.fromEntries(data.filter(r => r.parent === route.parent).map(r => [`'${r.method} ${r.path}'`, <typeof r>{
+          ...r,
+          method: `'${r.method}'`,
+          parent: `'${r.parent}'`,
+          path: `'${r.path}'`,
+        }]))
+      ];
+    })
+  ), undefined, 2
+).replaceAll("\"", "").replaceAll("'", "\"").replaceAll("},\n", "},\n\n");
+
+const text = `${COMPILED_OVERWRRITE_TEXT}\n\nexport type Routes = ${routes}\n\n${COMPILED_OVERWRRITE_TEXT}`;
+
+compiler.write(false)(text);
--- packages/frapi/src/routes.ts
+++ packages/frapi/src/routes.ts
@@ -1,65 +1,157 @@
 import type { Post, Response, User, CreatePost } from "@lafka/types";
 
+type SortBy =  "likes" | "dislikes" | "followers" | "created_at" | "changed_at";
+type SortTypes = "asc" | "desc" | 1 | -1 | true | false;
+
 // FOUTER__COMPILED__OVERWRITE_THIS \\
 
-type Routes = {
-  "GET users/": {
-    method: "GET",
-    path: "/",
-    parent: "users",
-    return: Response<User>,
-    arguments: {
-      query: {},
+export type Routes = {
+  "posts": {
+    "GET /": {
+      method: "GET",
+      path: "/",
+      parent: "posts",
+      return: Response<Post>,
+      arguments: {
+        query: {
+          offset?: number,
+          count?: number,
+          sortBy?: SortBy,
+          sortType?: SortTypes
+        },
 
-      body: {}
-    }
-  },
+        body?: {[key: string]: string}|undefined|null,
+        headers?: {[key: string]: string}|undefined|null
+      }
+    },
 
-  "GET users/:slug": {
-    method: "GET",
-    path: "/:slug",
-    parent: "users",
-    return: Response<User>,
-    arguments: {
-      query: {},
+    "GET /:id": {
+      method: "GET",
+      path: "/:id",
+      parent: "posts",
+      return: Response<Post>,
+      arguments: {
+        query?: undefined,
+        body?: {[key: string]: string}|undefined|null,
+        headers?: {[key: string]: string}|undefined|null
+      }
+    },
 
-      body: {}
-    }
-  },
+    "POST /": {
+      method: "POST",
+      path: "/",
+      parent: "posts",
+      return: Response<Post>,
+      arguments: {
+        headers: string,
+        body: CreatePost,
+        query?: {[key: string]: string}|undefined|null
+      }
+    },
 
-  "PUT users/:slug": {
-    method: "PUT",
-    path: "/:slug",
-    parent: "users",
-    return: Response<User>,
-    arguments: {
-      body: {},
+    "PUT /:id": {
+      method: "PUT",
+      path: "/:id",
+      parent: "posts",
+      return: Response<Post>,
+      arguments: {
+        headers: string,
+        body: Partial<Post>,
+        query?: {[key: string]: string}|undefined|null
+      }
+    },
 
-      headers: {
-        token: string
-      },
-
-      query: {
-        cache: boolean
+    "DELETE /:id": {
+      method: "DELETE",
+      path: "/:id",
+      parent: "posts",
+      return: Response<string>,
+      arguments: {
+        headers: string,
+        query?: {[key: string]: string}|undefined|null,
+        body?: {[key: string]: string}|undefined|null
       }
     }
   },
 
-  "DELETE users/:slug": {
-    method: "DELETE",
-    path: "/:slug",
-    parent: "users",
-    return: Response<User>,
-    arguments: {
-      headers: {
-        token: string
-      },
+  "users": {
+    "GET /": {
+      method: "GET",
+      path: "/",
+      parent: "users",
+      return: Response<User>,
+      arguments: {
+        query: boolean,
+        body?: {[key: string]: string}|undefined|null,
+        headers?: {[key: string]: string}|undefined|null
+      }
+    },
 
-      body: {},
+    "GET /:slug": {
+      method: "GET",
+      path: "/:slug",
+      parent: "users",
+      return: Response<User>,
+      arguments: {
+        query: boolean,
+        body?: {[key: string]: string}|undefined|null,
+        headers?: {[key: string]: string}|undefined|null
+      }
+    },
 
-      query: {}
+    "PUT /:slug": {
+      method: "PUT",
+      path: "/:slug",
+      parent: "users",
+      return: Response<User>,
+      arguments: {
+        body: Partial<User>,
+        headers: string,
+        query?: {[key: string]: string}|undefined|null
+      }
+    },
+
+    "DELETE /:slug": {
+      method: "DELETE",
+      path: "/:slug",
+      parent: "users",
+      return: Response<User>,
+      arguments: {
+        headers: string,
+        query?: {[key: string]: string}|undefined|null,
+        body?: {[key: string]: string}|undefined|null
+      }
     }
   }
 }
 
 // FOUTER__COMPILED__OVERWRITE_THIS \\
+
+export type Path<
+  Root extends keyof Routes,
+> = keyof Routes[Root];
+
+export type ParseRoute<
+  Root extends keyof Routes,
+  P extends Path<Root>
+> = {
+  //@ts-ignore
+  return: Routes[Root][P]["return"],
+  //@ts-ignore
+  path: `${Root}${Routes[Root][P]["path"]}`,
+  init: {
+    //@ts-ignore
+    body: Routes[Root][P]["body"],
+    //@ts-ignore
+    headers: Routes[Root][P]["headers"],
+    //@ts-ignore
+    query: Routes[Root][P]["query"],
+  }
+};
+
+export type RequestInitialize<
+  Root extends keyof Routes,
+  URL extends Path<Root>,
+  OmitData extends string = "query"
+> = Omit<Partial<RequestInit>, "body"|"headers">
+  & Omit<ParseRoute<Root, URL>["init"], OmitData>;
