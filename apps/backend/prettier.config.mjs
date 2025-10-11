381463f449e45bad9a17ce10eac2b805f2ad1c45
FOCKUSTY <116993925+FOCKUSTY@users.noreply.github.com>
2025-10-11 17:38:23 +0500

    пофиг, что сделал, это мои проблемы, если удалиться))))

--- apps/backend/prettier.config.mjs
+++
@@ -1,3 +0,0 @@
-import PrettierConfigLaf from "@lazy-and-focused/prettier-config";
-
-export default PrettierConfigLaf;
--- apps/frontend/prettier.config.mjs
+++
@@ -1,9 +0,0 @@
-import { PrettierLafConfiguration } from '@lazy-and-focused/prettier-config';
-
-/** @type {import("prettier").Config} */
-const config = {
-  ...PrettierLafConfiguration,
-  plugins: ['prettier-plugin-tailwindcss'],
-};
-
-export default config;
