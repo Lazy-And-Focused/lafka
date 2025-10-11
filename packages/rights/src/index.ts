9dad0435bd0cb7a8cfd475dec83c6e78fa81d5ea
FOCKUSTY <116993925+FOCKUSTY@users.noreply.github.com>
2025-10-11 18:16:59 +0500

    -style

--- packages/rights/src/index.ts
+++ packages/rights/src/index.ts
@@ -6,18 +6,17 @@ import { BitField } from "fbit-field";
 
 type MustArray<T, K = T> = [T, ...K[]];
 
-const AVAILABLE_RIGHTS = Rights.CONSTANTS.object.available
+const AVAILABLE_RIGHTS = Rights.CONSTANTS.object.available;
 const DEFAULT_RIGHTS = Rights.CONSTANTS.raw.default;
 
 const resolveArrayRightsToBigInt = <T extends Rights.Keys>(
   rightKey: T,
   ...rights: Rights.Rights<T>[]
 ) => {
-  const data = rights.map(key =>
-    AVAILABLE_RIGHTS[rightKey][key] as bigint);
+  const data = rights.map((key) => AVAILABLE_RIGHTS[rightKey][key] as bigint);
 
   return BitField.summarize(...data);
-}
+};
 
 type RightsOnly<
   T extends { rights: unknown; id: unknown },
@@ -36,7 +35,7 @@ export class UserService {
     const resolvedRights = resolveArrayRightsToBigInt("my", ...rights);
 
     return BitField.equals(this.user.rights, resolvedRights);
-  };
+  }
 
   public hasPostRights(post: RightsOnly<Post, "creator_id">) {
     return new PostService(post).userHas(this.user.id);
@@ -58,7 +57,7 @@ export class PostService {
 
   public hasRights<T extends keyof Rights.Posts>(
     ...rights: T[]
-  ): ((userId: string) => boolean) {
+  ): (userId: string) => boolean {
     const resolvedRights = resolveArrayRightsToBigInt("posts", ...rights);
 
     return (userId: string) => {
@@ -67,11 +66,11 @@ export class PostService {
 
       return BitField.equals(userRights, resolvedRights);
     };
-  };
+  }
 
   public userHas<T extends keyof Rights.Posts>(
     userId: string,
-  ): ((...rights: T[]) => boolean) {
+  ): (...rights: T[]) => boolean {
     const userRights = this.getUserRights(userId);
 
     return (...rights: T[]) => {
@@ -80,7 +79,7 @@ export class PostService {
       const resolvedRights = resolveArrayRightsToBigInt("posts", ...rights);
       return BitField.equals(userRights, resolvedRights);
     };
-  };
+  }
 
   public has<T extends keyof Rights.Posts>({
     rights,
@@ -91,7 +90,7 @@ export class PostService {
   }): boolean {
     if (this.post.creator_id === userId) return true;
     return this.hasRights(...rights)(userId);
-  };
+  }
 }
 
 export class OrganizationService {
@@ -103,37 +102,45 @@ export class OrganizationService {
   ) {}
 
   public getUserRights(userId: string) {
-    return BigInt(this.organization.members.includes(userId)
-      ? this.organization.rights.get(userId) || DEFAULT_RIGHTS.organizations
-      : DEFAULT_RIGHTS.organizations);
+    return BigInt(
+      this.organization.members.includes(userId)
+        ? this.organization.rights.get(userId) || DEFAULT_RIGHTS.organizations
+        : DEFAULT_RIGHTS.organizations,
+    );
   }
 
   public hasRights<T extends keyof Rights.Organizations>(
     ...rights: T[]
-  ): ((userId: string) => boolean) {
-    const resolvedRights = resolveArrayRightsToBigInt("organizations", ...rights);
+  ): (userId: string) => boolean {
+    const resolvedRights = resolveArrayRightsToBigInt(
+      "organizations",
+      ...rights,
+    );
 
     return (userId: string) => {
       if (this.organization.owner_id === userId) return true;
-      
+
       const userRights = this.getUserRights(userId);
       return BitField.equals(userRights, resolvedRights);
     };
-  };
+  }
 
   public userHas<T extends keyof Rights.Organizations>(
     userId: string,
-  ): ((...rights: T[]) => boolean) {
+  ): (...rights: T[]) => boolean {
     const userRights = this.getUserRights(userId);
 
     return (...rights: T[]) => {
       if (this.organization.owner_id === userId) return true;
 
-      const resolvedRights = resolveArrayRightsToBigInt("organizations", ...rights);
-      
+      const resolvedRights = resolveArrayRightsToBigInt(
+        "organizations",
+        ...rights,
+      );
+
       return BitField.equals(userRights, resolvedRights);
     };
-  };
+  }
 
   public has<T extends keyof Rights.Organizations>({
     rights,
@@ -145,5 +152,5 @@ export class OrganizationService {
     if (this.organization.owner_id === userId) return true;
 
     return this.hasRights(...rights)(userId);
-  };
+  }
 }
