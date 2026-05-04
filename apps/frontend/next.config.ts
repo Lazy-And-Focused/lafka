f0632f9bce00d95067701c6bcaf8880f22b5adf1
Valentin Bird <101190365+lanvalird@users.noreply.github.com>
2026-05-04 07:20:34 +0000

    feat!(frontend): recreate frontend

--- apps/frontend/next.config.ts
+++ apps/frontend/next.config.ts
@@ -1,16 +1,8 @@
-import type { NextConfig } from 'next';
+import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  // images: {
-  //   remotePatterns: [
-  //     {
-  //       protocol: 'https',
-  //       hostname: 'laf-info.netlify.app',
-  //       pathname: '/images/avatars/default.webp',
-  //     },
-  //   ],
-  // },
+  reactCompiler: true,
 };
 
 export default nextConfig;
---
+++ apps/old__frontend/src/app/page.tsx
@@ -0,0 +1,34 @@
+import Image from 'next/image';
+
+import MessageUser from '@/icons/message-user';
+
+export default async function HomePage() {
+  return (
+    <div className='section-block grid grid-cols-2'>
+      <div className='post'>
+        <div className='cover'>
+          <Image src='/images/placeholders/cover.webp' alt='Обложка поста' fill />
+
+          <p className='author'>
+            <MessageUser width={12} height={12} />
+            lanvalird
+          </p>
+        </div>
+
+        <div className='body'>
+          <p className='title' role='heading' aria-level={2}>
+            Пример поста
+          </p>
+          <p className='short'>
+            Мы вынуждены отталкиваться от того, что убеждённость некоторых
+            оппонентов прекрасно подходит для реализации соответствующих условий
+            активизации. Равным образом, сложившаяся структура организации
+            играет важную роль в формировании модели развития. Внезапно, явные
+            признаки победы институционализации неоднозначны и будут
+            заблокированы в рамках своих собственных рациональных ограничений.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
